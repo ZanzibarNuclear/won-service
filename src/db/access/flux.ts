089a7b9ef0fed08a400ee4beb8758db2cdbddf90
Zanzibar <zanzisworld42@gmail.com>
2024-10-27 16:11:39 -0700

    Finish refactoring query logic. Move to dedicated file.

--- src/db/access/flux.ts
+++ src/db/access/flux.ts
@@ -31,9 +31,16 @@ export const getFluxes = async () => {
     .execute()
 }
 
+export const getReplies = async (fluxId: number) => {
+  return await selectFluxQuery
+    .where('parent_id', '=', fluxId)
+    .orderBy('created_at', 'desc')
+    .execute()
+}
+
 // mutations
 export const createFlux = async (fluxUserId: number, parentId: number | null, content: string) => {
-  const flux = await db
+  return await db
     .insertInto('fluxes')
     .values({
       flux_user_id: fluxUserId,
@@ -42,12 +49,42 @@ export const createFlux = async (fluxUserId: number, parentId: number | null, co
     })
     .returning(['id'])
     .executeTakeFirst()
+}
 
-  return flux
+export const updateFlux = async (fluxId: number, content: string, authorId: number) => {
+  return await db
+    .updateTable('fluxes')
+    .set({ content })
+    .where('id', '=', fluxId)
+    .where('flux_user_id', '=', authorId)
+    .executeTakeFirst()
+}
+
+export const boostFlux = async (fluxId: number, fluxUserId: number) => {
+  await db
+    .insertInto('flux_boosts')
+    .values({ flux_id: fluxId, flux_user_id: fluxUserId })
+    .executeTakeFirst()
+  await recountFluxBoosts(fluxId)
+  return await getFlux(fluxId)
+}
+
+export const deboostFlux = async (fluxId: number, fluxUserId: number) => {
+  await db
+    .deleteFrom('flux_boosts')
+    .where('flux_id', '=', fluxId)
+    .where('flux_user_id', '=', fluxUserId)
+    .executeTakeFirst()
+  await recountFluxBoosts(fluxId)
+  return await getFlux(fluxId)
+}
+
+export const recountFluxBoosts = async (fluxId: number) => {
+  const boostCount = await db.selectFrom('flux_boosts').select('count(*)').where('flux_id', '=', fluxId).executeTakeFirst()
+  await db.updateTable('fluxes').set({ boost_count: boostCount?.count }).where('id', '=', fluxId).executeTakeFirst()
 }
 
 // subscriptions
-
 
 // identity
 export const getFluxUser = async (userId: string) => {
--- src/routes/api/fluxes/index.ts
+++ src/routes/api/fluxes/index.ts
@@ -1,13 +1,26 @@
 import { FastifyPluginAsync } from 'fastify'
-import { db } from '../../../db/Database'
-import { getFluxes, getFlux, createFlux, getFluxUser } from '../../../db/access/flux'
+import {
+  getFluxes,
+  getFlux,
+  createFlux,
+  getFluxUser,
+  updateFlux,
+  boostFlux,
+  deboostFlux,
+  getReplies,
+} from '../../../db/access/flux'
 
 const fluxesRoutes: FastifyPluginAsync = async (fastify, options) => {
   fastify.get('/', async (request, reply) => {
+
+    // TODO: replace filtering logic
+    // TODO: add pagination
+
     return await getFluxes()
   })
 
   fastify.post('/', async (request, reply) => {
+    // TODO: check this in one place for all guarded routes -- need way to know which are guarded
     if (!request.session?.userId) {
       fastify.log.warn(`Only known users may post fluxes`)
       return reply.status(401).send({ error: 'Unauthorized' })
@@ -17,7 +30,7 @@ const fluxesRoutes: FastifyPluginAsync = async (fastify, options) => {
       fastify.log.warn(`Flux user not found for current user ${request.session.userId}`)
       return reply.status(401).send({ error: 'Unauthorized' })
     }
-    const { parentId, content } = request.body as { parentId: number | null, content: string }
+    const { parentId, content } = request.body as { parentId: number | null; content: string }
     const flux = await createFlux(author.id, parentId, content)
     if (!flux) {
       fastify.log.error(`Failed to create flux for user ${author.id}`)
@@ -32,25 +45,42 @@ const fluxesRoutes: FastifyPluginAsync = async (fastify, options) => {
   })
 
   fastify.put('/:fluxId', async (request, reply) => {
-    const { fluxId } = request.params as { fluxId: string }
+    if (!request.session?.userId) {
+      fastify.log.warn(`Only known users may post fluxes`)
+      return reply.status(401).send({ error: 'Unauthorized' })
+    }
+    const author = await getFluxUser(request.session.userId)
+    if (!author) {
+      fastify.log.warn(`Flux user not found for current user ${request.session.userId}`)
+      return reply.status(401).send({ error: 'Unauthorized' })
+    }
+    const { fluxId } = request.params as { fluxId: number }
     const { content } = request.body as { content: string }
-    const flux = await db.updateTable('fluxes').set({ content }).where('id', '=', Number(fluxId)).executeTakeFirst()
+    const flux = await updateFlux(fluxId, content, author.id)
     return flux
   })
 
+  fastify.get('/:fluxId/replies', async (request, reply) => {
+
+    // TODO: add pagination
+
+    const { fluxId } = request.params as { fluxId: number }
+    return await getReplies(fluxId)
+  })
+
   fastify.post('/:fluxId/boost', async (request, reply) => {
-    const { fluxId } = request.params as { fluxId: string }
-    const { userId } = request.session as { userId: string }
+    if (!request.session?.userId) {
+      fastify.log.warn(`Only known users may post fluxes`)
+      return reply.status(401).send({ error: 'Unauthorized' })
+    }
+    const author = await getFluxUser(request.session.userId)
+    if (!author) {
+      fastify.log.warn(`Flux user not found for current user ${request.session.userId}`)
+      return reply.status(401).send({ error: 'Unauthorized' })
+    }
+    const { fluxId } = request.params as { fluxId: number }
     try {
-      const fluxUser = await db.selectFrom('flux_users').select('id').where('user_id', '=', userId).executeTakeFirst()
-      if (!fluxUser) {
-        return reply.status(404).send({ error: 'Flux user not found' })
-      }
-      const fluxUserId = fluxUser.id
-      await db.insertInto('flux_boosts').values({
-        flux_id: Number(fluxId),
-        flux_user_id: fluxUserId,
-      }).execute()
+      return await boostFlux(fluxId, author.id)
     } catch (error) {
       console.error(error)
       if ((error as any).code === '23505') {
@@ -61,22 +91,22 @@ const fluxesRoutes: FastifyPluginAsync = async (fastify, options) => {
   })
 
   fastify.delete('/:fluxId/boost', async (request, reply) => {
-    const { fluxId } = request.params as { fluxId: string }
-    const { userId } = request.session as { userId: string }
-    const fluxUser = await db.selectFrom('flux_users').select('id').where('user_id', '=', userId).executeTakeFirst()
-    if (!fluxUser) {
-      return reply.status(404).send({ error: 'Flux user not found' })
+    if (!request.session?.userId) {
+      fastify.log.warn(`Only known users may post fluxes`)
+      return reply.status(401).send({ error: 'Unauthorized' })
     }
-    const fluxUserId = fluxUser.id
-    const boost = await db.deleteFrom('flux_boosts').where('flux_id', '=', Number(fluxId)).where('flux_user_id', '=', fluxUserId).execute()
-    fastify.log.info({ boost }, 'Unboosted flux')
-    return reply.status(200).send({ message: 'Flux unboosted' })
-  })
-
-  fastify.get('/:fluxId/replies', async (request, reply) => {
-    const { fluxId } = request.params as { fluxId: string }
-    const replies = await db.selectFrom('fluxes').selectAll().where('parent_id', '=', Number(fluxId)).execute()
-    return replies
+    const author = await getFluxUser(request.session.userId)
+    if (!author) {
+      fastify.log.warn(`Flux user not found for current user ${request.session.userId}`)
+      return reply.status(401).send({ error: 'Unauthorized' })
+    }
+    const { fluxId } = request.params as { fluxId: number }
+    try {
+      return await deboostFlux(fluxId, author.id)
+    } catch (error) {
+      console.error(error)
+      return reply.status(500).send({ message: 'Failed to unboost flux' })
+    }
   })
 }
 
