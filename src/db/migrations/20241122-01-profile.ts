e941a1763b8f844e52e23830ce0e617ca1f05450
Zanzibar <zanzisworld42@gmail.com>
2024-11-22 09:01:42 -0800

    Fix migration issue

--- src/db/migrations/20241122-01-profile.ts
+++ src/db/migrations/20241122-01-profile.ts
@@ -4,15 +4,12 @@ export async function up(db: Kysely<any>): Promise<void> {
   await db.schema
     .alterTable('profiles')
     .dropColumn('username')
-    .renameColumn('screen_name', 'alias')
-    .renameColumn('created_at', 'joined_at')
     .addColumn('email', 'varchar')
     .addColumn('email_verified_at', 'timestamp')
     .execute()
 
   await db.schema
     .alterTable('flux_users')
-    .renameColumn('created_at', 'joined_at')
     .addColumn('email_notifications', 'timestamp')
     .addColumn('text_notifications', 'timestamp')
     .addColumn('digest_frequency', 'varchar')
@@ -24,14 +21,11 @@ export async function down(db: Kysely<any>): Promise<void> {
     .dropColumn('digest_frequency')
     .dropColumn('text_notifications')
     .dropColumn('email_notifications')
-    .renameColumn('joined_at', 'created_at')
     .execute()
 
   await db.schema.alterTable('profiles')
     .dropColumn('email_verified_at')
     .dropColumn('email')
-    .renameColumn('joined_at', 'created_at')
-    .renameColumn('alias', 'screen_name')
     .addColumn('username', 'varchar')
     .execute()
 }
