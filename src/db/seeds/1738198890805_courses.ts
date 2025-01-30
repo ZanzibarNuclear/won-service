5d14984ef52d01f55434b8d2e7f02254a7f12632
Zanzibar <zanzisworld42@gmail.com>
2025-01-29 17:05:53 -0800

    Use generated seed file

--- src/db/seeds/1738198890805_courses.ts
+++ src/db/seeds/1738198890805_courses.ts
@@ -1,6 +1,8 @@
+import courses from '../etf/Courses'
+import lessonPlans from '../etf/LessonPlans'
 import type { Kysely } from 'kysely'
 
 export async function seed(db: Kysely<any>): Promise<void> {
-	// seed code goes here...
-	// note: this function is mandatory. you must implement this function.
+	await courses.copyData()
+	await lessonPlans.copyData()
 }
--- src/db/seeds/20250129-02-learning.ts
+++
@@ -1,8 +0,0 @@
-import type { Kysely } from 'kysely'
-import courses from '../etf/Courses'
-import lessonPlans from '../etf/LessonPlans'
-
-export async function seed(db: Kysely<any>): Promise<void> {
-  await courses.copyData()
-  await lessonPlans.copyData()
-}
