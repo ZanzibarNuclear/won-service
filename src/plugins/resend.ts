6d26ae857605dceb118f27474bd8143d62f69949
Zanzibar <zanzisworld42@gmail.com>
2025-03-26 18:41:54 -0700

    Some debugging for the server

--- src/plugins/resend.ts
+++ src/plugins/resend.ts
@@ -8,26 +8,32 @@ const resend: FastifyPluginAsync = async (fastify, options) => {
   const resendFeedback = new Resend(fastify.config.RESEND_FEEDBACK_KEY)
 
   // ultimate flexibility
-  fastify.decorate('resendAuth', resend)
+  fastify.decorate('resendAuth', resendAuth)
   fastify.decorate('resendFeedback', resendFeedback)
 
   // most common use case (?)
   fastify.decorate('sendAuthEmail', async (from: string, to: string, subject: string, htmlBody: string) => {
-    return await resendAuth.emails.send({
+    fastify.log.info('sending auth email: ' + subject)
+    const resendResponse = await resendAuth.emails.send({
       from,
       to,
       subject,
       html: htmlBody
     })
+    fastify.log.info(JSON.stringify(resendResponse))
+    return resendResponse
   })
 
   fastify.decorate('sendFeedbackEmail', async (from: string, to: string, subject: string, htmlBody: string) => {
-    return await resendFeedback.emails.send({
+    fastify.log.info('sending feedback email: ' + subject)
+    const resendResponse = await resendFeedback.emails.send({
       from,
       to,
       subject,
       html: htmlBody
     })
+    fastify.log.info(JSON.stringify(resendResponse))
+    return resendResponse
   })
 
   fastify.log.info('registered resend plugin')
