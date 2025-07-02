a391286944555703cddd74456025e7d85b54c1d6
Zanzibar <zanzisworld42@gmail.com>
2025-07-02 11:09:47 -0700

    Revert to single file route declarations. Auto route path building does not work

--- src/routes/api/adv/storylines/index.ts
+++ src/routes/api/adv/storylines/index.ts
@@ -1,5 +1,20 @@
 import { FastifyPluginAsync, FastifyRequest, FastifyReply } from 'fastify'
-import { Storyline } from '../../../../models/storyline.schema'
+import { Storyline, Chapter } from '../../../../models/storyline.schema'
+
+interface StorylineParams {
+  Params: { slId: string }
+}
+
+interface ChapterParams {
+  Params: {
+    slId: string,
+    chId: string
+  }
+}
+
+interface ChapterBody {
+  Body: Partial<Chapter>
+}
 
 interface StorylineBody {
   Body: Partial<Storyline>
@@ -8,7 +23,7 @@ interface StorylineBody {
 const storylineRoutes: FastifyPluginAsync = async (fastify, options) => {
 
   fastify.get(
-    '/',
+    '',
     async (request: FastifyRequest, reply: FastifyReply) => {
       const storylines = await fastify.models.storyline.list()
       return reply.send(storylines)
@@ -16,12 +31,61 @@ const storylineRoutes: FastifyPluginAsync = async (fastify, options) => {
   )
 
   fastify.post<StorylineBody>(
-    '/',
+    '',
     async (request: FastifyRequest<StorylineBody>, reply: FastifyReply) => {
       const storyline = await fastify.models.storyline.create(request.body)
       return reply.code(201).send(storyline)
     },
   )
+
+  fastify.get(
+    '/:slId',
+    async (request: FastifyRequest<StorylineParams>, reply: FastifyReply) => {
+      const storylines = await fastify.models.storyline.findById(request.params.slId)
+      return reply.send(storylines)
+    },
+  )
+
+  fastify.patch(
+    '/:slId',
+    async (request: FastifyRequest<StorylineParams & StorylineBody>, reply: FastifyReply) => {
+      const storylines = await fastify.models.storyline.updateStoryline(request.params.slId, request.body)
+      return reply.send(storylines)
+    },
+  )
+
+  fastify.post<StorylineParams & ChapterBody>(
+    '/:slId/chapters',
+    async (request: FastifyRequest<StorylineParams & ChapterBody>, reply: FastifyReply) => {
+      const chapter = await fastify.models.storyline.addChapter(request.params.slId, request.body)
+      return reply.code(201).send(chapter)
+    },
+  )
+
+  fastify.get<StorylineParams>(
+    '/:slId/chapters',
+    async (request: FastifyRequest<StorylineParams>, reply: FastifyReply) => {
+      const chapters = await fastify.models.storyline.getChapters(request.params.slId)
+      return reply.send(chapters)
+    },
+  )
+
+  fastify.get<ChapterParams>(
+    '/:slId/chapters/:chId',
+    async (request: FastifyRequest<ChapterParams>, reply: FastifyReply) => {
+      const chapters = await fastify.models.chapter.getById(request.params.slId, request.params.chId)
+      return reply.send(chapters)
+    },
+  )
+
+  fastify.patch<ChapterParams & ChapterBody>(
+    '/:slId/chapters/:chId',
+    async (request: FastifyRequest<ChapterParams & ChapterBody>, reply: FastifyReply) => {
+      const chapters = await fastify.models.chapter.update(request.params.slId, request.params.chId, request.body)
+      return reply.send(chapters)
+    },
+  )
+
 }
 
 export default storylineRoutes
